501de4771e49d6763eee8ae30a10db7a5d5ab004
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2021-11-23 17:34:01 +0100

    Add main.py

---
+++ mine.py
@@ -0,0 +1,21 @@
+z2_numbers_list = list(range(1,50))
+
+#z2_numbers_list = [1, 2, 3, 5, 6, 11, 12, 18, 19, 21, ]
+primenumbers = list()
+flag = False
+
+z2_numbers_list.sort()
+last_element = z2_numbers_list[-1]
+
+for i in z2_numbers_list:
+  flag = False
+  if i > 1:
+    for x in range (2,last_element):
+      if (i > x):
+        if ( i % x == 0):
+          flag = True
+          break
+  if not flag and i > 1:
+    primenumbers.append(i)
+
+print (primenumbers)
